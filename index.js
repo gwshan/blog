7731cb9f0ddad5c28dca629f6ed286b773d441c7
Gavin Shan <gwshan@linux.vnet.ibm.com>
2016-11-24 16:12:15 +1100

    virt/kvm: add virt/VM-KVM_CREATE_VCPU.txt
    Signed-off-by: Gavin Shan <gwshan@linux.vnet.ibm.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var MenuStrings = new Array(
     "Kernel->generic->jump label->kernel/generic/jump_label.txt",
-    "Virtualization->kvm->CHAR-KVM_CREATE_VM->virt/CHAR-KVM_CREATE_VM.txt",
+    "Virtualization->KVM->CHAR-KVM_CREATE_VM->virt/CHAR-KVM_CREATE_VM.txt",
+    "Virtualization->KVM->VM-KVM_CREATE_VCPU->virt/VM-KVM_CREATE_VCPU.txt",
     "openBMC->uboot->Palmetto bootup->openbmc/uboot/palmetto_bootup.txt",
     "openBMC->uboot->bss/data/rodata section->openbmc/uboot/bss_data_rodata.txt",
     "openBMC->uboot->Command [ping]->openbmc/uboot/command_ping.txt",
